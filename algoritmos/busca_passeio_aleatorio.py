25825f3a4ab4a1d358cace34df7de44804c80c5b
pablojguedes <pablo_j_guedes@hotmail.com>
2024-12-08 12:58:38 -0300

    busca passeio aleatorio corrigido para verificar os nós certos

--- algoritmos/busca_passeio_aleatorio.py
+++ algoritmos/busca_passeio_aleatorio.py
@@ -6,19 +6,24 @@ def busca_passeio_aleatorio(grafo, no_inicial, recurso, ttl, config):
     arestas_percorridas = []  # Lista para armazenar as arestas percorridas
     no_atual = no_inicial
 
-    while ttl > 0:
-        if no_atual in visitados:
-            continue
+    while ttl >= 0:
         visitados.add(no_atual)
         mensagens += 1
         if recurso in config['resources'].get(no_atual, []):
             return visitados, mensagens, arestas_percorridas, True
         vizinhos = list(grafo.neighbors(no_atual))
-        if not vizinhos:
-            break
         proximo_no = random.choice(vizinhos)
-        arestas_percorridas.append((no_atual, proximo_no))  # Armazena a aresta
-        no_atual = proximo_no
+        while(proximo_no in visitados and len(vizinhos) > 0):
+            vizinhos.remove(proximo_no)
+            if(len(vizinhos) > 0):
+                proximo_no = random.choice(vizinhos)
+       
+        # if(len(vizinhos) == 0 and len() != len(visitados)):
+        #     proximo_no = random.choice(vizinhos)
+        
         ttl -= 1
+        if(ttl >= 0):
+            arestas_percorridas.append((no_atual, proximo_no))  # Armazena a aresta
+            no_atual = proximo_no
 
     return visitados, mensagens, arestas_percorridas, False
